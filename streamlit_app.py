eaf0af24f56cfd0cb645d2b2b98434f8cea6c9d1
CAROL BUENAVENTURA <121153654+carolbuenaventura@users.noreply.github.com>
2022-12-30 11:00:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,35 @@
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# delete - streamlit.text(fruityvice_response.json()) #json writes data to the screen
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+streamlit.title('My Moms New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmel')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#Display the table on the page
+streamlit.dataframe(fruits_to_show)
+
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
 
 
